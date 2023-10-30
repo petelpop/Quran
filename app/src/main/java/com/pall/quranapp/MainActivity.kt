e1dbdaa659ce6a1e164cb8945d81e90712e60c9f
ipal <72238450+petelpop@users.noreply.github.com>
2023-10-30 08:34:27 +0700

    add raw fragment and graph and sync the id

---
+++ app/src/main/java/com/pall/quranapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pall.quranapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.damarazka.quran.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

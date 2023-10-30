bd5f4f9bc2eb2c8a93d139f922536cc4a5e37051
ipal <72238450+petelpop@users.noreply.github.com>
2023-10-30 09:26:32 +0700

    edit DetailSurahActivity.kt

--- app/src/main/java/com/pall/quranapp/presentation/quran/DetailSurahActivity.kt
+++ app/src/main/java/com/pall/quranapp/presentation/quran/DetailSurahActivity.kt
@@ -9,50 +9,46 @@ import com.pall.quranapp.adapter.SurahAdapter
 import com.damarazka.quran.databinding.ActivityDetailSurahBinding
 
 
-class DetailSurahActivity {
+class DetailSurahActivity : AppCompatActivity() {
+    private var _binding: ActivityDetailSurahBinding? = null
+    private val binding get() = _binding as ActivityDetailSurahBinding
 
-    class DetailSurahActivity : AppCompatActivity() {
-        private var _binding: ActivityDetailSurahBinding? = null
-        private val binding get() = _binding as ActivityDetailSurahBinding
+    private var _surah: SurahItem? = null
+    private val surah get() = _surah as SurahItem
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityDetailSurahBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        private var _surah: SurahItem? = null
-        private val surah get() = _surah as SurahItem
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            _binding = ActivityDetailSurahBinding.inflate(layoutInflater)
-            setContentView(binding.root)
+        _surah = intent.getParcelableExtra(EXTRA_DATA, SurahItem::class.java)
 
-            _surah = intent.getParcelableExtra(EXTRA_DATA, SurahItem::class.java)
+        initView()
+        val mAdapter = SurahAdapter()
 
-            initView()
-            val mAdapter = SurahAdapter()
-
-
-            val quranViewModel = ViewModelProvider(this)[QuranViewModel::class.java]
-            surah.number?.let { quranViewModel.getListAyahBySurah(it) }
-            quranViewModel.listAyah.observe(this){
-                mAdapter.setData(it.quranEdition?.get(0)?.ayahs, it.quranEdition)
-                binding.rvSurah.apply {
-                    adapter = mAdapter
-                    layoutManager = LinearLayoutManager(this@DetailSurahActivity)
-                }
+        val quranViewModel = ViewModelProvider(this)[QuranViewModel::class.java]
+        surah.number?.let { quranViewModel.getListAyahBySurah(it) }
+        quranViewModel.listAyah.observe(this){
+            mAdapter.setData(it.quranEdition?.get(0)?.ayahs, it.quranEdition)
+            binding.rvSurah.apply {
+                adapter = mAdapter
+                layoutManager = LinearLayoutManager(this@DetailSurahActivity)
             }
         }
+    }
 
-        private fun initView() {
-            binding.apply {
-                tvDetailSurah.text = surah.englishName
-                tvDetailNameTranslation.text = surah.englishNameTranslation
-                val revelationSurah = surah.revelationType
-                val numberAyahs = surah.numberOfAyahs
-                val resultAyah = "$revelationSurah - $numberAyahs"
-                tvDetailAyah.text = resultAyah
-                tvDetailName.text = surah.name
-            }
+    private fun initView() {
+        binding.apply {
+            tvDetailSurah.text = surah.englishName
+            tvDetailNameTranslation.text = surah.englishNameTranslation
+            val revelationSurah = surah.revelationType
+            val numberAyahs = surah.numberOfAyahs
+            val resultAyah = "$revelationSurah - $numberAyahs"
+            tvDetailAyah.text = resultAyah
+            tvDetailName.text = surah.name
         }
+    }
 
-        companion object {
-            const val EXTRA_DATA = "extra_data"
-        }
+    companion object {
+        const val EXTRA_DATA = "extra_data"
     }
 }

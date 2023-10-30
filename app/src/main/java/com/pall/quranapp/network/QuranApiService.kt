f4167f00ded7c14f167290458b4b1defee7c38ad
ipal <72238450+petelpop@users.noreply.github.com>
2023-10-30 08:41:42 +0700

    add network API

---
+++ app/src/main/java/com/pall/quranapp/network/QuranApiService.kt
@@ -0,0 +1,9 @@
+package com.pall.quranapp.network
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface QuranApiService {
+    @GET("surah")
+    fun getListSurah() : Call<SurahItem>
+}
---
+++ app/src/main/java/com/pall/quranapp/network/SurahResponse.kt
@@ -0,0 +1,38 @@
+package com.pall.quranapp.network
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class SurahResponse(
+
+    @Json(name="code")
+    val code: Int? = null,
+
+    @Json(name="data")
+    val listSurah: List<SurahItem>? = null,
+
+    @Json(name="status")
+    val status: String? = null
+)
+
+data class SurahItem(
+
+    @Json(name="number")
+    val number: Int? = null,
+
+    @Json(name="englishName")
+    val englishName: String? = null,
+
+    @Json(name="numberOfAyahs")
+    val numberOfAyahs: Int? = null,
+
+    @Json(name="revelationType")
+    val revelationType: String? = null,
+
+    @Json(name="name")
+    val name: String? = null,
+
+    @Json(name="englishNameTranslation")
+    val englishNameTranslation: String? = null
+)
